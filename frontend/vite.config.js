1e8b1bf49edeadbacb59753951bccb54051c225a
kaye2512 <kayeromy@gmail.com>
2026-03-24 21:30:15 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
       '/api': 'http://localhost:8000',
     },
+  },
+  preview: {
+    port: 5173, // ← même port que server
+    host: '0.0.0.0',
+    allowedHosts: 'all',
   },
 });
